6232ca26996407e289319c4a40a6fbe9bfe6ab0d
Sakaa4Short <klanyenek@gmail.com>
2023-12-13 13:56:08 -0800

    first level of mob coding

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import Square from './components/Square'
 import "./App.css"
 
 const App = () => {
@@ -14,9 +15,31 @@ const App = () => {
     "?"
   ])
 
+
+  // console.log("board: ", board)
+
+  const handleClick = (id) => {
+    let treasureLocation = Math.floor(Math.random() * board.length)
+    let bombLocation = Math.floor(Math.random() * board.length)
+   
+    
+    if (treasureLocation === id) {
+      board[id] = "💎"
+      setBoard([...board])
+    } else if(bombLocation === treasureLocation || bombLocation === id) {
+      board[id] = "💣"
+      setBoard([...board])
+    } else {
+      board[id] = "💰"
+      setBoard([...board])
+    }
+
+    }
+  
   return (
     <>
       <h1>Treasure Hunt Game</h1>
+      <Square board={board} handleClick={handleClick}/>
     </>
   )
 }
